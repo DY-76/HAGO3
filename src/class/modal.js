e6c5a6e7a6d6c58df007a66f6a5f9c1c95ad6769
dongyub <9754745@naver.com>
2020-02-27 19:40:40 +0900

    Update modal.js

--- src/class/modal.js
+++ src/class/modal.js
@@ -9,10 +9,11 @@
         var span = document.getElementsByClassName("close")[0];                                          
  
         // When the user clicks on the button, open the modal 
+        /*
         btn.onclick = function() {
             modal.style.display = "block";
         }
- 
+        */
         // When the user clicks on <span> (x), close the modal
         span.onclick = function() {
             modal.style.display = "none";
