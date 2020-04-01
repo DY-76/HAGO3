8377b9f3b6db59a09f8be5436525ab42a6b4b74d
dongyub <9754745@naver.com>
2020-04-01 15:19:41 +0900

    병합
    1

--- src/class/modal.js
+++ src/class/modal.js
@@ -28,6 +28,5 @@
             if (event.target == OBmodal) {
                 OBmodal.style.display = "none";
                 $("#DPS-tab").get(0).click();
-                $("#showall-tab").get(0).click();
             }
         }
