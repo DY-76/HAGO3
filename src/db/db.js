0b8eb58a3476b914b4742c250449bc49d1cba9bf
dongyub <9754745@naver.com>
2020-04-27 17:38:47 +0900

    1
    1

--- src/db/db.js
+++ src/db/db.js
@@ -1,26 +1,31 @@
+'use strict';
 
-var mysql=require('mysql')
+var mysql = require('mysql');
 
-var connection=mysql.createConnection({
+var connection = mysql.createConnection({
     host:'localhost',
     user:'root',
     password:'jjuny1977',
     port:'3309',
     database:'hagoproject'
 });
-export function scan_db(){
-connection.connect();
 
-connection.query('select * from user', function(err,row,fields){
-    if(!err)
-    console.log(rows);
-    else
-    console.log('Error!',err);
+export function scan_db(){
+
+    connection.connect();
+
+    connection.query('select * from user', function(err, row, fields){
+        if(!err) {
+            console.log(rows);
+        }
+        else {
+            console.log('Error!', err);
+        }
 
 });
 
 connection.end();
-};
-export function db_compare(){
 
+}
+export function db_compare(){
 }
