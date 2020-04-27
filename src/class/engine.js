964dd2974094312b3944f91bc2a880f463a6765a
dongyub <9754745@naver.com>
2020-04-27 18:36:15 +0900

    1
    1

--- src/class/engine.js
+++ src/class/engine.js
@@ -6,7 +6,7 @@
 import { GEHelper } from '../graphicEngine/GEHelper';
 import { alone_stage1 } from '../stage/alone_stage';
 import * as add from '../object/objects.js';
-import { db_c } from  '../db/db.js';
+import { scandb } from  '../db/db.js';
 
 var i = 'off';
 var test;
@@ -937,7 +937,7 @@ document.getElementById("table").innerHTML += "<tr><td>파일수정날짜</td><t
 }
 
 click_db(){
-    db_c.scandb();
+    scandb();
 }
 
 fileread(){
