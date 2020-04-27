2146764e32cec8628bf8bd5db346332e4f74de42
dongyub <9754745@naver.com>
2020-04-27 16:58:12 +0900

    1
    1

--- src/stage/stagemanager.js
+++ src/stage/stagemanager.js
@@ -1,6 +1,26 @@
 'use strict';
 import * as alone_stage from './alone_stage.js';
 
+function getParam(sname) {
+    var params = location.search.substr(location.search.indexOf("?") + 1);
+
+    var sval = "";
+
+    params = params.split("&");
+
+    for (var i = 0; i < params.length; i++) {
+        var temp = params[i].split("=");
+        if ([temp[0]] == sname) {
+            sval = temp[1];
+        }
+    }
+    return sval;
+}
+
+console.log(getParam("stage"));
+console.log(getParam("id"));
+console.log(getParam("pw"));
+
 export function Stage_out(name,i){
     switch (name) {
         case 'alone':
