618091d2c694deefdd29bab3daa531ca58a04976
JSH1995 <44968599+JSH1995@users.noreply.github.com>
2020-04-27 20:43:04 +0900

    1
    1

--- src/db/index.js
+++ src/db/index.js
@@ -55,5 +55,7 @@ app.get('/user', function(req, res){
 });
 
 app.listen(app.get('port'), function () {
+  
   console.log('Express server listening on port ' + app.get('port'));
+  console.log(rows);
 });
