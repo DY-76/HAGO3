25fed68b50fe2797493cf3532a3fec60d05f836b
dongyub <9754745@naver.com>
2020-04-28 16:58:47 +0900

    Update DB_router.js
    1111

--- DB/DB_router.js
+++ DB/DB_router.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
@@ -16,8 +15,6 @@ var connection = mysql.createConnection({
   function (err, rows, fields) {
       if (!err){
       console.log('The solution is: ', rows);
-
-      var data = rows;
       }
     else{
       console.log('Error while performing Query.', err);
@@ -33,7 +30,15 @@ router.use(function timeLog(req, res, next) {
 });
 // define the home page route
 router.get('/', function(req, res) {
-  res.send('제발 ㅠㅠ\n',data);
+  res.send('제발 ㅠㅠ\n');
+  connection.query('SELECT * FROM user', (err, rows) => {
+    connection.release(); // 연결세션 반환.
+    if (err) {
+      throw err;
+    }
+
+    return res.send({ data: rows }); // 결과는 rows에 담아 전송
+  });
 });
 // define the about route
 router.get('/about', function(req, res) {
