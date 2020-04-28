0e20670394c36dcb2888d2a456d501404edec192
dongyub <9754745@naver.com>
2020-04-28 18:51:51 +0900

    Update app.js
    1

--- DB/app.js
+++ DB/app.js
@@ -6,8 +6,8 @@ const api = require('./DB_router');
 app.use(bodyParser.json());
 app.use('/DB', api);
 
+app.set('views', '../example');
+app.set('view engine', 'ejs');
+
 const port = process.env.PORT || 5050;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
-
-app.set('views', './example');
-app.set('view engine', 'ejs');
