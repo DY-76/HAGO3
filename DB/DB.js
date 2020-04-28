32b2a388e730fde05e72cf1317d0cdbb3c83939d
dongyub <9754745@naver.com>
2020-04-28 15:29:33 +0900

    1
    1

--- DB/DB.js
+++ DB/DB.js
@@ -1,4 +1,9 @@
 var mysql      = require('mysql');
+var express    = require('express');
+var app        = express();
+
+app.use('/DB', require('./DB_router'));
+
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
@@ -9,10 +14,11 @@ var connection = mysql.createConnection({
 connection.connect();
 
 connection.query('SELECT * FROM user', 
-function (error, results, fields) {
-  if (error) throw error;
-  console.log('DB_test', results);
-});
-
+function (err, rows, fields) {
+    if (!err)
+    console.log('The solution is: ', rows);
+  else
+    console.log('Error while performing Query.', err);
+})
 
 connection.end();
