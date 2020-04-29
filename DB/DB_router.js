66143a10cce6bc9051ba49bd0a6e9e53240364e7
dongyub <9754745@naver.com>
2020-04-29 14:48:59 +0900

    Update DB_router.js
    1

--- DB/DB_router.js
+++ DB/DB_router.js
@@ -60,7 +60,7 @@ router.get('/about', function(req, res) {
 });
 
 router.get('/id', function(req, res) {
-  var tagId = req.query.id+'';
+  var tagId = req.query.id;
   connection.query('select EXISTS (select * from User where User_Id='+tagId+') as success', 
   function (err, result, fields) {
       if (!err){
