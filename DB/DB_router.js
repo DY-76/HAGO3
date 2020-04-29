c24e108c0adf2be8696bf3b2672a74ad9d2abdfe
dongyub <9754745@naver.com>
2020-04-29 14:26:00 +0900

    1
    1

--- DB/DB_router.js
+++ DB/DB_router.js
@@ -61,12 +61,13 @@ router.get('/about', function(req, res) {
 
 router.get('/id', function(req, res) {
   var tagId = req.query.id;
-  connection.query('select EXISTS (select * from User where User_Id='+string(tagId)+') as success', 
+  connection.query('select EXISTS (select * from User where User_Id='+tagId+') as success', 
   function (err, result, fields) {
       if (!err){
       
-      console.log(tagId);// 1234
-      console.log(req.query.id);// 1234
+      console.log(tagId); // tagId 들어갔는지
+      console.log(req.query.id);// 인식된 값 확인
+      
       res.render( 'mid' , {DBdata:tagId,
                          All:JSON.stringify(result)
                         });
