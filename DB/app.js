297e94793d692585f32675386105f6574e7b31d9
JSH1995 <44968599+JSH1995@users.noreply.github.com>
2020-05-05 22:32:26 +0900

    1
    1

--- DB/app.js
+++ DB/app.js
@@ -18,12 +18,6 @@ app.use(session({
     saveUninitialized:true,
     store: sessionStore                                       
   }))
-  app.get('/', function(req, res){
-    ​	if(req.session.authId)
-    ​		res.send(authId+'님 로그인되었습니다');
-    ​	else
-    ​		res.send('로그인하세요!');
-    });
 
 app.set('views',__dirname+'/views');
 //app.set('view engine', 'ejs');
