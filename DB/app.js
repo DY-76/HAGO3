1ff1a0722271c5caa6dc868f5b5cf3dcc4beca7c
JSH1995 <44968599+JSH1995@users.noreply.github.com>
2020-05-05 21:52:41 +0900

    1
    1

--- DB/app.js
+++ DB/app.js
@@ -2,14 +2,29 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
 const api = require('./DB_router');
-
+var session = require('express-session');  
+var MySQLStore = require('express-mysql-session')(session);
+var options ={                                               
+    host     : 'localhost',
+    user     : 'root',
+    password : 'jjuny1977',
+    database : 'hagoproject',
+    port     : 3306
+};
+var sessionStore = new MySQLStore(options);  
+app.use(session({                                             
+    secret:"asdfasffdas",
+    resave:false,
+    saveUninitialized:true,
+    store: sessionStore                                       
+  }))
 
 app.set('views',__dirname+'/views');
-app.set('view engine', 'ejs');
+//app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use('/DB', api);
 
 
-
+  
 const port = process.env.PORT || 5050;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
