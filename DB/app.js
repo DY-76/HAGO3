50999c6191ae0ae9b9b74708977ef5ed0dc5accb
dongyub <9754745@naver.com>
2020-05-10 15:05:27 +0900

    Update app.js
    1

--- DB/app.js
+++ DB/app.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.set('views',__dirname+'/views');
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
-app.use('', api);
+app.use('/a', api);
   
 const port = process.env.PORT || 5050;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
