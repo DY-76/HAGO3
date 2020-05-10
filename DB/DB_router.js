a598a63352063962fbe59b5be622b4a7e2462a88
JSH1995 <44968599+JSH1995@users.noreply.github.com>
2020-05-10 14:43:25 +0900

    1
    1

--- DB/DB_router.js
+++ DB/DB_router.js
@@ -37,16 +37,7 @@ router.use(function timeLog(req, res, next) {
   console.log('Time: ', Date.now());
   next();
 });
-router.get('', function(req, res) {
-  res.render('mid');
 // define the home page route
-router.get('', function(req, res){
-  res.render( 'test' , {DBdata:'Done!',
-                         All:'test'
-                        });
-});
-
-
 router.get('/all', function(req, res) {
     res.render( 'mid' , {DBdata:'Done!',
                          All:'test'
