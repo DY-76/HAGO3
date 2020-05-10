cceeec9203f5538957b1725b407164c0173ea849
JSH1995 <44968599+JSH1995@users.noreply.github.com>
2020-05-10 14:56:40 +0900

    1
    1

--- DB/app.js
+++ DB/app.js
@@ -2,35 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 const app = express();
-const app2 = express();
 const api = require('./DB_router');
-const test_api = require('./test_router');
 var session = require('express-session');  
 var MySQLStore = require('express-mysql-session')(session);
-
-var http = require('http');
-var options = {
-    hostname: 'codinghago.com',
-    port:'3389',
-    path: '/ip'
-  };
- 
-function handleResponse(response) {
-  var serverData = '';
-  response.on('data', function (chunk) {
-    serverData += chunk;
-  });
-  response.on('end', function () {
-    console.log("received server data:");
-    console.log(serverData);
-  });
-}
- 
-http.request(options, function(response){
-  handleResponse(response);
-}).end();
-
-
 var options ={                                               
     host     : 'localhost',
     user     : 'root',
@@ -51,20 +25,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use('/DB', api);
 
-var sessionStore = new MySQLStore(options);  
-app2.use(session({                                             
-    secret:"asdfasffdas",
-    resave:false,
-    saveUninitialized:true,
-    store: sessionStore                                       
-  }))
-app2.use(cookieParser());
-app2.set('views',__dirname+'/views');
-app2.set('view engine', 'ejs');
-app2.use(bodyParser.json());
-app2.use('', test_api);
+app.use('',api);
   
 const port = process.env.PORT || 5050;
-const port2 = process.env.PORT || 5151;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
-app2.listen(port2, () => console.log(`Listening on port ${port2}...`));
