5b3dd868bd501ec928cb7f3d9ea201d8c29ff1e1
dongyub <9754745@naver.com>
2020-05-10 14:53:34 +0900

    Update app.js
    1

--- DB/app.js
+++ DB/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 const app = express();
+const app2 = express();
 const api = require('./DB_router');
 const test_api = require('./test_router');
 var session = require('express-session');  
@@ -25,7 +26,20 @@ app.set('views',__dirname+'/views');
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use('/DB', api);
-app.use('', test_api);
+
+var sessionStore = new MySQLStore(options);  
+app2.use(session({                                             
+    secret:"asdfasffdas",
+    resave:false,
+    saveUninitialized:true,
+    store: sessionStore                                       
+  }))
+app2.use(cookieParser());
+app2.set('views',__dirname+'/views');
+app2.set('view engine', 'ejs');
+app2.use(bodyParser.json());
+app2.use('', test_api);
   
 const port = process.env.PORT || 5050;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
+app2.listen(port, () => console.log(`Listening on port ${port}...`));
