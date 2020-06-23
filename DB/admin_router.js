897c718579a9ed40516c1c4328540987d578712e
dongyub <9754745@naver.com>
2020-06-23 20:10:11 +0900

    Update admin_router.js
    1

--- DB/admin_router.js
+++ DB/admin_router.js
@@ -161,7 +161,7 @@ router.post('/contents', function(req, res) {
   connection.query("SELECT Contents_Json FROM condetails WHERE ConDetails_No = "+req.body.ContentsNo,
   function (err, result, fields) {
       if (!err){
-        var test_go = result[0]['Contents_Json'];
+        var test_go = ''+result[0]['Contents_Json'];
         var responseData = {'result' : 'ok', 'test' : test_go, 'type' : 'load'};
         console.log(result);
       }
